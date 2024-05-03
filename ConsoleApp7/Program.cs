fbd7935072a267bea0784cd77c4339003aff0fc1
User <User@MAXDUST>
2024-05-03 20:18:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static int Stones(string J, string S)
+    {
+        HashSet<char> jewelTypes = new HashSet<char>(J);
+        int count = 0;
+        foreach (char stone in S)
+        {
+            if (jewelTypes.Contains(stone))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static void Main()
+    {
+        string J = "i";
+        string S = "jksdfgiasdy";
+        int result = Stones(J, S);
+        Console.WriteLine(result); 
+    }
+}
